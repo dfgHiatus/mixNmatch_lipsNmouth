ea9f0750fde3d5ac7d630d45a1700bd6c0f98404
dfgHiatus <evansBM@protonmail.com>
2023-10-10 08:15:48 -0700

    Update for Resonite

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # mixNmatch_lipsNmouth
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/)  
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/)  
 Fixes Issue 3340, allows for differing makes and models of Eye and Face Trackers to operate together.
 
-https://github.com/Neos-Metaverse/NeosPublic/issues/3440
+https://github.com/Resonite-Metaverse/ResonitePublic/issues/3440
 
 ## Usage
-0. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Build and place your DLL under "nml_mods". This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader.
+0. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+1. Build and place your DLL under "rml_mods". This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods` for a default install. You can create it if it's missing, or if you launch the game once with ResoniteModLoader.
 2. Start the game!
 
 Thanks to those who helped me test this!
--- mixNmatch_lipsNmouth/mixNmatch_lipsNmouth.cs
+++ mixNmatch_lipsNmouth/mixNmatch_lipsNmouth.cs
@@ -1,57 +1,53 @@
 ﻿using HarmonyLib;
-using NeosModLoader;
+using ResoniteModLoader;
 using FrooxEngine;
 using FrooxEngine.CommonAvatar;
 
-namespace mixNmatch_lipsNmouth
+namespace mixNmatch_lipsNmouth;
+
+public class mixNmatch_lipsNmouth : ResoniteMod
 {
-	public class mixNmatch_lipsNmouth : NeosMod
+	public override string Name => "mixNmatch_lipsNmouth";
+	public override string Author => "dfgHiatus";
+	public override string Version => "2.0.0";
+	public override string Link => "https://github.com/dfgHiatus/mixNmatch_lipsNmouth";
+	public override void OnEngineInit()
 	{
-		public override string Name => "mixNmatch_lipsNmouth";
-		public override string Author => "dfgHiatus";
-		public override string Version => "1.0.2";
-		public override string Link => "https://github.com/dfgHiatus/mixNmatch_lipsNmouth";
-		public override void OnEngineInit()
-		{
-			// Harmony.DEBUG = true;
-			Harmony harmony = new Harmony("net.dfgHiatus.mixNmatch_lipsNmouth");
-			harmony.PatchAll();
-		}
+		new Harmony("net.dfgHiatus.mixNmatch_lipsNmouth").PatchAll(); 
+	}
 
-		// Fix Issue 3440 (Can't mix and match the Eye Tracker with SRAnipal Lip)
-		// Merge EIA485's fix
-		[HarmonyPatch(typeof(AvatarEyeDataSourceAssigner), "OnEquip")]
-		private static class AvatarEyeDataSourceAssignerPatch
+	// Fixes Issue 3440 (Can't mix and match the Eye Tracker with SRAnipal Lip)
+	[HarmonyPatch(typeof(AvatarEyeDataSourceAssigner), "OnEquip")]
+	private static class AvatarEyeDataSourceAssignerPatch
+	{
+		private static bool Prefix(AvatarEyeDataSourceAssigner __instance, AvatarObjectSlot slot)
 		{
-			private static bool Prefix(AvatarEyeDataSourceAssigner __instance, AvatarObjectSlot slot)
-			{
-				if (__instance.TargetReference.Target == null)
-					return false;
-				AvatarEyeTrackingInfo rawEyeData = null;
-				slot.Slot.ActiveUserRoot.ForeachRegisteredComponent<AvatarEyeTrackingInfo>(val => {
-					if (val.EyeDataSource.Target?.IsEyeTrackingActive ?? false)
-						rawEyeData = val;
-				});
-				__instance.TargetReference.Target.Target = rawEyeData?.EyeDataSource.Target;
+			if (__instance.TargetReference.Target == null)
 				return false;
-			}
+			AvatarEyeTrackingInfo rawEyeData = null;
+			slot.Slot.ActiveUserRoot.ForeachRegisteredComponent<AvatarEyeTrackingInfo>(val => {
+				if (val.EyeDataSource.Target?.IsEyeTrackingActive ?? false)
+					rawEyeData = val;
+			});
+			__instance.TargetReference.Target.Target = rawEyeData?.EyeDataSource.Target;
+			return false;
 		}
+	}
 
-		[HarmonyPatch(typeof(AvatarMouthDataSourceAssigner), "OnEquip")]
-		private static class AvatarMouthDataSourceAssignerPatch
+	[HarmonyPatch(typeof(AvatarMouthDataSourceAssigner), "OnEquip")]
+	private static class AvatarMouthDataSourceAssignerPatch
+	{
+		private static bool Prefix(AvatarMouthDataSourceAssigner __instance, AvatarObjectSlot slot)
 		{
-			private static bool Prefix(AvatarMouthDataSourceAssigner __instance, AvatarObjectSlot slot)
-			{
-				if (__instance.TargetReference.Target == null)
-					return false;
-				AvatarMouthTrackingInfo rawMouthData = null;
-				slot.Slot.ActiveUserRoot.ForeachRegisteredComponent<AvatarMouthTrackingInfo>(val => {
-					if ((val.MouthDataSource.Target as MouthTrackingStreamManager)?.IsTracking.Target.Value ?? false)
-						rawMouthData = val;
-				});
-				__instance.TargetReference.Target.Target = rawMouthData?.MouthDataSource.Target;
+			if (__instance.TargetReference.Target == null)
 				return false;
-			}
+			AvatarMouthTrackingInfo rawMouthData = null;
+			slot.Slot.ActiveUserRoot.ForeachRegisteredComponent<AvatarMouthTrackingInfo>(val => {
+				if ((val.MouthDataSource.Target as MouthTrackingStreamManager)?.IsTracking.Target.Value ?? false)
+					rawMouthData = val;
+			});
+			__instance.TargetReference.Target.Target = rawMouthData?.MouthDataSource.Target;
+			return false;
 		}
 	}
 }
